80faad61d83a036384630d10ddee39955673de7f
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2020-05-11 22:44:00 +0200

    Add support for specifying a subset of repositories to be included in a tarball.

--- pack_repos.sh
+++ pack_repos.sh
@@ -2,11 +2,21 @@
 
 formatted_date=$(date +%Y%m%d_%H%M)
 
-temp_tar=$(pwd)/repos_$formatted_date.tar
+if [[ $# -gt 0 ]]; then
+  for gid in "$@"; do
+    group_dirs="$group_dirs group_$gid"
+  done
+  repos_trail=_not_complete_
+else
+  group_dirs=group_*
+  repos_trail=_
+fi
+
+temp_tar=$(pwd)/repos${repos_trail}${formatted_date}.tar
 tar -cf "$temp_tar" -T /dev/null
 
 cd repos
-for group in group_*; do
+for group in $group_dirs; do
   echo $group
   if [[ -f "$group" ]]; then
     tar -rf "$temp_tar" "$group"
